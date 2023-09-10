62455b9a2273cc59f4bbc9e9c875bf8c3da4352d
udoudoima <okonsaviour38@gmail.com>
2023-09-10 01:42:50 -0400

    responsive

---
+++ index.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById("btn");
+let her = document.getElementById('her');
+let cont = document.getElementById('cont');
+
+
+function toggleBtn() {
+    btn.classList.toggle("active");
+    her.classList.toggle('here');
+    her.classList.toggle('conti');
+
+
+
+}
